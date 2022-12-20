e793e7b651190fae35f6c4dc7e0eee4fe70d80f6
Salmo Mascarenhas <salmo.cruz@gmail.com>
2022-12-20 06:45:17 -0300

    rename and modulate file.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+
+<?php include("./partials/head.php"); ?>
+
+<body class="conteudo">
+    <?php include_once("./partials/game.php") ?>
+    <?php include_once("./partials/scripts.php"); ?>
+</body>
+
+</html>
